161a3a1a138d1fd924ba2313c3d280fc80729508
shimaa fathi <shimaafathi126@gmail.com>
2023-12-15 18:31:33 +0200

    Update dbms.sh

--- dbms.sh
+++ dbms.sh
@@ -150,6 +150,158 @@ function listTable() {
 } # End listTable function.
 
 # Function to drop a table
+#!/bin/bash
+
+# ================================<< Start of (( Directory Variables )) >>================================
+
+# Directory to store databases (same as the script file directory)
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+DATABASE_DIR="$SCRIPT_DIR/databases"
+
+# another DATABASE_DIR possible to use (makes databases stored in the user home directory)
+# DATABASE_DIR="/home/$(whoami)/Databases/"
+
+# Create the directory if it doesn't exist
+mkdir -p "$DATABASE_DIR"
+
+# Variable to track the current database
+currentDb=""
+
+# ================================<< End of (( Directory Variables )) >>================================
+
+# ================================<< Start of (( Functions of DBMS )) >>================================
+
+# Function to create a new database
+function createDatabase() {
+    read -p "Enter the database name: " dbName
+    dbPath="$DATABASE_DIR/$dbName"
+    if [[ ! $dbName =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
+        echo "Invalid database name. Database names must start with a letter or underscore and only contain letters, numbers, and underscores."
+    elif [ -d "$dbPath" ]; then
+        echo "Database '$dbName' already exists."
+    else
+        mkdir "$dbPath"
+        echo "Database '$dbName' created successfully."
+    fi
+}
+
+# Function to list all databases
+function listDatabase() {
+    echo "Available databases:"
+    for db in "$DATABASE_DIR"/*/; do
+        echo "- $(basename "${db%/}")"
+    done
+}
+
+# Function to connect to a database
+function connectToDatabase() {
+    read -p "Enter the database name: " dbName
+    dbPath="$DATABASE_DIR/$dbName"
+    if [ -d "$dbPath" ]; then
+        currentDb="$dbPath"
+        echo "Connected to database '$dbName'."
+    else
+        echo "Database '$dbName' not found."
+    fi
+}
+
+# Function to drop a database
+function dropDatabase() {
+    read -p "Enter the database name to drop: " dbName
+    dbPath="$DATABASE_DIR/$dbName"
+    if [ -d "$dbPath" ]; then
+        rm -r "$dbPath"
+        echo "Database '$dbName' dropped successfully."
+        currentDb=""
+    else
+        echo "Database '$dbName' not found."
+    fi
+}
+
+# Function to create a new table
+function createTable() {
+    echo "createTable function is called."
+    
+    if [ -z "$currentDb" ]
+    then
+        echo "No database selected. Please connect to a database first."
+        return
+    fi
+    
+    echo -n "Enter the table name: "
+    read tableName
+
+    if [ -z "$tableName" ]
+    then
+        echo "Table name cannot be empty. Aborting table creation."
+        return
+    fi
+    
+    if echo "$tableName" | grep -qE '^[a-zA-Z0-9_]+$'
+    then
+        echo "Valid table name."
+    else
+        echo "Invalid characters in the table name. Please use only alphanumeric characters and underscores."
+        return
+    fi
+
+    tablePath="$currentDb/$tableName"
+
+    if [ -e "$tablePath" ]
+    then
+        echo "Table '$tableName' already exists in the current database. Do you want to overwrite it? (y/n): "
+        read -r overwrite
+        if [ "$overwrite" != "y" ]; then
+            echo "Table creation canceled."
+            return
+        fi
+    fi
+
+    echo -n "Enter column names (comma-separated): "
+    read columns
+    if [ -z "$columns" ]
+    then
+        echo "Column names cannot be empty. Aborting table creation."
+        return
+    fi
+    uniqueColumns=$(echo "$columns" | tr ',' '\n' | sort -u | tr '\n' ',' | sed 's/,$//')
+
+    if [ "$columns" != "$uniqueColumns" ]
+    then
+        echo "Error: Duplicate column names found. Aborting table creation."
+        return
+    fi
+
+    touch "$tablePath"
+    echo "$columns" > "$tablePath"
+    echo "Table '$tableName' created successfully in the current database."
+} # End createTable function.
+
+
+
+
+
+
+# Function to list all tables in the current database
+function listTable() {
+    echo "listTable function is called."
+    DATABASE_DIR="$SCRIPT_DIR/databases"
+    cd "$DATABASE_DIR" || { echo "Error: Could not change to the database directory."; return; }
+    echo "Tables in the current database:"
+    
+    if [ -z "$(sudo ls -A *)" ]
+    then
+        echo "No tables found in the current database."
+        return
+    fi
+    
+    for table in *
+    do
+        echo "- ${table}"
+    done
+} # End listTable function.
+
+# Function to drop a table
 function dropTable() {
     echo "dropTable function is called."
     
@@ -183,7 +335,7 @@ function dropTable() {
     else
         echo "Table '$tableName' not found in the current database."
     fi
-} #End drop table function
+} #End drop table function 
 
 
 # Function to insert into a table
